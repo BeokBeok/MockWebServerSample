4904dc1899a16ef7379f9517c0a111d3940f9dbc
강현석 <kekemusa37@gmail.com>
2021-09-30 16:03:32 +0900

    :bug: [수정] Retrofit Annotation 누락

--- app/src/main/java/com/beok/mockwebserversample/FooService.kt
+++ app/src/main/java/com/beok/mockwebserversample/FooService.kt
@@ -1,8 +1,9 @@
 package com.beok.mockwebserversample
 
 import retrofit2.Call
+import retrofit2.http.GET
 
 interface FooService {
-    // Error occur
+    @GET("foo/")
     fun getFoo(): Call<Foo>
 }
